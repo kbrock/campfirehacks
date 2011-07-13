ec784a0e65d094f115f6a57a2ba9db29f5e1ef46
Keenan Brock <kbrock@patientslikeme.com>
2011-07-13 08:47:37 -0400

    add welcome / description page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Campfirehacks::Application.routes.draw do
   resources :sheets
+  root :to => 'welcome#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
