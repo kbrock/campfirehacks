074608b5fb43867c83e4590b193f9a4bd6740f35
Keenan Brock <kbrock@patientslikeme.com>
2011-09-08 10:06:51 -0400

    add a user script that has embedded style in it

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -4,8 +4,17 @@ class ScriptsController < ApplicationController
     params[:id] =~ /([0-9]+)\.user/
     @id = $1 || params[:id]
 
+    @me = Avatar.find_by_username(@id) rescue nil
+  end
+
+  def show2
+    params[:id] =~ /([0-9]+)\.user/
+    @id = $1 || params[:id]
+
     #TODO: only pull back those that I want to ignore
-    #@components=Component.all
+    @components=Component.all
+
+    @avatars = Avatar.all
 
     @me = Avatar.find_by_username(@id) rescue nil
   end
