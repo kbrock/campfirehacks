b72b7be7e7ba8baef99fd3a1f949bd4b1d3b86c0
Keenan Brock <kbrock@patientslikeme.com>
2011-09-08 10:05:48 -0400

    use @id variable in sheets controller (more similar to scripts controller)

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,11 +1,14 @@
 class SheetsController < ApplicationController
 
   def show
+    params[:id] =~ /([0-9]+)\.user/
+    @id = $1 || params[:id]
+
     #TODO: only pull back those that I want to ignore
     @components=Component.all
 
     @avatars = Avatar.all
-    @me = Avatar.find_by_username(params[:id]) rescue nil
+    @me = Avatar.find_by_username(@id) rescue nil
 
   end
 end
